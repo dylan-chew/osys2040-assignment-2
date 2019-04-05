712a5092bbaad9a6e07592fb0be0b14948ccedcf
Dylan Chew <dylanw.chew@gmail.com>
2019-04-05 15:50:54 -0300

    messing with database

--- src/utils/PostgresUtil.js
+++ src/utils/PostgresUtil.js
@@ -1,10 +1,10 @@
 const { Pool } = require('pg')
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  username: "admin",
-  password: "password",
-  database: "admin"
+  connectionString: process.env.DATABASE_URL
+  // username: "admin",
+  // password: "password",
+  // database: "admin"
   // ssl: true,
 })
 
