5cd409015b6a848ec89d0f273972b9cde56ee8fb
Dylan Chew <dylanw.chew@gmail.com>
2019-04-05 15:52:45 -0300

    messing with postgresutil

--- src/utils/PostgresUtil.js
+++ src/utils/PostgresUtil.js
@@ -1,11 +1,11 @@
 const { Pool } = require('pg')
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL
-  // username: "admin",
-  // password: "password",
-  // database: "admin"
-  // ssl: true,
+  connectionString: process.env.DATABASE_URL,
+  username: "admin",
+  password: "password",
+  database: "admin"
+  ssl: true,
 })
 
 module.exports = {
