a8a32b5a4038281a02b71943fe9550d60415a6ec
Dylan Chew <dylanw.chew@gmail.com>
2019-04-08 16:30:08 -0300

    created new database in heroku app. pushing changes from local app to web app

--- src/utils/PostgresUtil.js
+++ src/utils/PostgresUtil.js
@@ -2,10 +2,10 @@ const { Pool } = require('pg')
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  username: "admin",
-  password: "password",
-  database: "admin"
-  // ssl: true,
+  // username: "admin",
+  // password: "password",
+  // database: "admin"
+  ssl: true,
 })
 
 module.exports = {
